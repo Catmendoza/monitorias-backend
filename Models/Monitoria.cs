2568675be944e22522ca74e87cc679198c0a5ab2
catmendoza <mendo657@gmail.com>
2020-11-27 01:20:32 -0500

    validacion

--- Models/Monitoria.cs
+++ Models/Monitoria.cs
@@ -1,6 +1,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace Monitorias.Models
 {
@@ -9,23 +10,30 @@ namespace Monitorias.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-
+        [Required(ErrorMessage = "El nombre es requerido")]
         [BsonElement("Name")]
         [JsonProperty("Name")]
         public string name { get; set; }
+        [Required(ErrorMessage = "La facultad es requerida")]
+        [StringLength(80)]
 
         public string faculty { get; set; }
+        [Required(ErrorMessage = "El día es requerido")]
 
         public string day { get; set; }
+        [Required(ErrorMessage = "La hora de inicio es requerida")]
         public string init { get; set; }
+        [Required(ErrorMessage = "La hora de finalización requerida")]
         public string end { get; set; }
+        [Required(ErrorMessage = "El numero de salón o link de la sala son requeridos")]
 
         public string livingRoom { get; set; }
+        [Required(ErrorMessage = "La disponibilidad de cupos es requerida")]
         public int availableQuotas { get; set; }
         public int initialQuotas { get; set; }
         public string description { get; set; }
 
         public string[] students { get; set; }
         public string monitor { get; set; }
-     }
+    }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -10,21 +10,24 @@ namespace Monitorias.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(50)]
         [BsonElement("Name")]
         [JsonProperty("Name")]
 
         public string name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El programa es requerido")]
         public string career { get; set; }
         [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
         public string mail { get; set; }
-        [Required]
+        [Required(ErrorMessage = "La cedula es requerido")]
         public string code { get; set; }
-        [Required]
+        [Required(ErrorMessage = "La contraseña es requerido")]
+        [StringLength(8, ErrorMessage = "La contraseña tiene que tener maximo 8 caracteres.")]
         public string password { get; set; }
-        [Required]
-        public int roll {get; set;}
-        
+
+        public int roll { get; set; }
+
     }
 }
